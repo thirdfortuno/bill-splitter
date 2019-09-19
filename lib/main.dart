5c73a1482bf7c9a23e2c739ccb8596db5bc6fd84
Pio Fortuno III <thirdfort@gmail.com>
2019-09-19 16:41:28 +0800

    added basic input logic

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,34 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int numPeople = 0;
+  final _node = FocusNode();
+  final _controller = TextEditingController();
 
+  @override
+  void initState(){
+    super.initState();
+    _controller.addListener(_peopleListener);
+    _node.addListener(_peopleListener);
+  }
+
+  void _peopleListener(){
+    setState(() {
+      if(_node.hasFocus){
+        numPeople = int.parse(_controller.value.text);
+      }
+      if (!_node.hasFocus){
+        _controller.value = _controller.value.copyWith(text: numPeople.toString());
+      }
+    });
+  }
+
+  @override
+  void dispose(){
+    _node.removeListener(_peopleListener);
+    _node.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,13 +72,19 @@ class _MyHomePageState extends State<MyHomePage> {
                         Icons.chevron_left,
                       ),
                       onPressed: (){
-
+                        _node.unfocus();
+                        if(numPeople > 0){
+                          numPeople -= 1;
+                        } 
+                        _peopleListener();
                       },
                     ),
                     Container(
                       height: 40,
                       width: 50,
                       child: TextField(
+                        focusNode: _node,
+                        controller: _controller,
                         textAlign: TextAlign.center,
                         keyboardType: TextInputType.number,
                       ),
@@ -61,12 +94,21 @@ class _MyHomePageState extends State<MyHomePage> {
                         Icons.chevron_right,
                       ),
                       onPressed: (){
-
+                        _node.unfocus();
+                        numPeople += 1;
+                        _peopleListener();
                       },
                     ),
                     Spacer()
                   ],
                 ),
+                SizedBox(height: 20),
+                InkWell(
+                  child: Text("Next Page"),
+                  onTap: (){
+                    print("$numPeople");
+                  },
+                )
               ],
             ),
           ),
